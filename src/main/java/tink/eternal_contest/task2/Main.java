7deeeaa03bd95c23fe1159b814a5460129d7e559
Tunebaker <tunebaker@gmail.com>
2023-08-20 23:20:43 +0300

    tink.task2 solution

--- src/main/java/tink/eternal_contest/task2/Main.java
+++ src/main/java/tink/eternal_contest/task2/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int parts = scanner.nextInt();
-        int cuts = (int) Math.ceil(Math.log(parts)/Math.log(2));
+        int needParts = scanner.nextInt();
+        int cuts = 0;
+        int gotParts = 1;
+        while(gotParts < needParts){
+            gotParts *= 2;
+            cuts++;
+        }
         System.out.println(cuts);
     }
 }
