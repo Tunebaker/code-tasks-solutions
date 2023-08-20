aa8e16ec4ea163333c7839803b93752a4218487a
Tunebaker <tunebaker@gmail.com>
2023-08-20 13:34:41 +0300

    kyu6.handshake_problem research - for cycle replaced by stream

--- src/main/java/codewars/kyu6/handshake_problem/Kata.java
+++ src/main/java/codewars/kyu6/handshake_problem/Kata.java
@@ -1,12 +1,12 @@
 package codewars.kyu6.handshake_problem;
 
+import java.util.stream.IntStream;
+
 public class Kata {
     public static int GetParticipants(int handshakes) {
         int participant = 5;
         int maxShakes = 0;
-        for (int i = participant; i > 0; i--) {
-            maxShakes += i - 1;
-        }
+        maxShakes = IntStream.range(1, participant).sum();
         System.out.printf("for %d participant maxShakes is %d \n", participant, maxShakes);
         return 0; //TODO
     }
