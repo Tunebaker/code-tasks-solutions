98c4b7527f0331a37b37ad53957a00b7504be812
Tunebaker <tunebaker@gmail.com>
2023-08-20 14:05:29 +0300

    kyu6.handshake_problem working solution #1

--- src/main/java/codewars/kyu6/handshake_problem/Kata.java
+++ src/main/java/codewars/kyu6/handshake_problem/Kata.java
@@ -4,10 +4,15 @@ import java.util.stream.IntStream;
 
 public class Kata {
     public static int GetParticipants(int handshakes) {
-        int participant = 5;
+        int participant = 0;
         int maxShakes = 0;
-        maxShakes = IntStream.range(1, participant).sum();
-        System.out.printf("for %d participant maxShakes is %d \n", participant, maxShakes);
-        return 0; //TODO
+
+        while (handshakes > maxShakes) {
+            participant++;
+            maxShakes = IntStream.range(1, participant).sum();
+            System.out.printf("for %d participant maxShakes is %d \n", participant, maxShakes);
+        }
+
+        return participant;
     }
 }
